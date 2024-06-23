ca8cb1100a95027929cbfc9649e90bf1ff5cf890
Sayan Das <thegeek.sayan@gmail.com>
2024-06-24 00:16:47 +0530

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
-import Approach from "@/components/Approach";
-import Clients from "@/components/Clients";
-import Experience from "@/components/Experience";
-import Footer from "@/components/Footer";
-import Grid from "@/components/Grid";
-import Hero from "@/components/Hero";
-import RecentProjects from "@/components/RecentProjects";
-import { FloatingNav } from "@/components/ui/FloatingNav";
-import { navItems } from "@/data";
+import Approach from "../components/Approach";
+import Clients from "../components/Clients";
+import Experience from "../components/Experience";
+import Footer from "../components/Footer";
+import Grid from "../components/Grid";
+import Hero from "../components/Hero";
+import RecentProjects from "../components/RecentProjects";
+import { FloatingNav } from "../components/ui/FloatingNav";
+import { navItems } from "../data";
 import Image from "next/image";
 import { FaHome } from "react-icons/fa";
 import Contact from "../components/Contact";
-import { CardHoverEffect } from "@/components/HoverEffectCards";
-import PreLoader from "@/components/Preloader";
+import { CardHoverEffect } from "../components/HoverEffectCards";
+import PreLoader from "../components/Preloader";
 
 export default function Home() {
   return (
