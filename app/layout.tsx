83b15091af2f60ad7ec7184aa0f28feb14704553
Sayan Das <thegeek.sayan@gmail.com>
2024-06-23 10:01:29 +0530

    remove google font in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      {/* <body className={inter.className}> */}
+      <body>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
